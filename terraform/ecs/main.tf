a4e5a90d9f2f42ee40f51bbfe75898ccac1abceb
Brandon Willett <bcwillett1@gmail.com>
2020-07-18 17:48:53 -0400

    add a few more terraforms

---
+++ terraform/ecs/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  version = "~> 2.70"
+  region  = "us-east-2"
+}
+
+resource "aws_ecs_cluster" "check_cluster" {
+  name               = "check-cluster"
+  capacity_providers = ["FARGATE_SPOT"]
+}
+
+resource "aws_ecs_task_definition" "check_s3" {
+  family                = "service"
+  container_definitions = file("./check-s3-definition.json")
+
+  task_role_arn = "arn:aws:iam::880716778824:role/ecs-role"
+
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+
+  cpu    = 512
+  memory = 1024
+}
