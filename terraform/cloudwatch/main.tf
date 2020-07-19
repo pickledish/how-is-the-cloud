a3fbd2f8ec0b549f588efca245de3a9b73ef85d1
Brandon Willett <bcwillett1@gmail.com>
2020-07-19 14:07:01 -0400

    set up scheduled ECS with new checker

--- terraform/cloudwatch/main.tf
+++ terraform/cloudwatch/main.tf
@@ -7,12 +7,12 @@ resource "aws_cloudwatch_event_rule" "every_five_minutes" {
   name        = "every-five-minutes"
   description = "A rule that fires every five minutes"
 
-  is_enabled          = false
+  is_enabled          = true
   schedule_expression = "rate(5 minutes)"
 }
 
-resource "aws_cloudwatch_event_target" "ecs_target" {
-  target_id = "ecs-target"
+resource "aws_cloudwatch_event_target" "check_cloudflare" {
+  target_id = "check-cloudflare"
   arn       = "arn:aws:ecs:us-east-2:880716778824:cluster/check-cluster"
   rule      = aws_cloudwatch_event_rule.every_five_minutes.name
   role_arn  = aws_iam_role.cloudwatch_role.arn
@@ -22,7 +22,7 @@ resource "aws_cloudwatch_event_target" "ecs_target" {
     platform_version = "1.4.0"
 
     task_count          = 1
-    task_definition_arn = "arn:aws:ecs:us-east-2:880716778824:task-definition/service:1"
+    task_definition_arn = "arn:aws:ecs:us-east-2:880716778824:task-definition/check-cloudflare-task:1"
 
     network_configuration {
       subnets          = data.aws_subnet_ids.default.ids
--- terraform/ecs/main.tf
+++ terraform/ecs/main.tf
@@ -8,15 +8,19 @@ resource "aws_ecs_cluster" "check_cluster" {
   capacity_providers = ["FARGATE_SPOT"]
 }
 
-resource "aws_ecs_task_definition" "check_s3" {
-  family                = "service"
-  container_definitions = file("./check-s3-definition.json")
+resource "aws_ecs_task_definition" "check_cloudflare" {
+  family                = "check-cloudflare-task"
+  container_definitions = file("./check-cloudflare-definition.json")
 
   task_role_arn = aws_iam_role.ecs_role.arn
 
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
 
-  cpu    = 512
-  memory = 1024
+  cpu    = 256
+  memory = 512
+}
+
+output "check_cloudflare_task_definition_arn" {
+  value = aws_ecs_task_definition.check_cloudflare.arn
 }
