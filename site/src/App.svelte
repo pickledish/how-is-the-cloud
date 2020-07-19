44e00f4bf81229782ec45ed8bca9290580b2f6f3
Brandon Willett <bcwillett1@gmail.com>
2020-07-18 23:44:38 -0400

    cleaning

--- site/src/App.svelte
+++ site/src/App.svelte
@@ -5,26 +5,21 @@
   export let name;
 </script>
 
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-</style>
-
 <Tailwindcss />
-<main>
-  <h1 class="text-2xl">
+
+<main class="w-full px-2 md:px-32">
+  <h1 class="text-4xl mb-4">
     How is the Cloud?
   </h1>
-  <h3 class="text-lg">
+  <h3 class="text-xl mb-4">
     Stop being gaslit by ___ status pages and find out if critical cloud services are down using real API calls.
   </h3>
-  <div class="flex justify-between">
-    <Service name="Docker Hub"/>
-    <Service name="Cloudflare"/>
-    <Service name="Amazon S3"/>
-    <Service name="GitHub"/>
+  <div class="w-full flex justify-center">
+    <div class="flex justify-between w-full md:w-10/12">
+      <Service name="Docker Hub"/>
+      <Service name="Cloudflare"/>
+      <Service name="Amazon S3"/>
+      <Service name="GitHub"/>
+    </div>
   </div>
 </main>
