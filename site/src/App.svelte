6ed45917ca10731bf451adc5c160e6eeb9c1ff8b
Brandon Willett <bcwillett1@gmail.com>
2020-07-18 23:05:06 -0400

    frontend first commit

---
+++ site/src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Tailwindcss from "./Tailwindcss.svelte";
+  import Service from './Service.svelte'
+
+  export let name;
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+</style>
+
+<Tailwindcss />
+<main>
+  <h1 class="text-2xl">
+    How is the Cloud?
+  </h1>
+  <h3 class="text-lg">
+    Stop being gaslit by ___ status pages and find out if critical cloud services are down using real API calls.
+  </h3>
+  <div class="flex justify-between">
+    <Service name="Docker Hub"/>
+    <Service name="Cloudflare"/>
+    <Service name="Amazon S3"/>
+    <Service name="GitHub"/>
+  </div>
+</main>
---
+++ site/src/Service.svelte
@@ -0,0 +1,21 @@
+<script>
+
+	export let name;
+
+</script>
+
+<div class="w-48 h-48 border-solid border-4 border-gray-600 flex flex-col justify-between items-stretch p-3">
+	<div class="flex justify-between items-center mb-1">
+		<div class="font-bold">{name}</div>
+		<svg class="fill-current w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1.25 17.292l-4.5-4.364 1.857-1.858 2.643 2.506 5.643-5.784 1.857 1.857-7.5 7.643z"/></svg>
+	</div>
+	<div class="flex-auto border-solid border-4 border-gray-600">
+		<div class="max-h-full">
+			PHOTO
+			<!-- <img class="object-contain" src="docker.png"/> -->
+		</div>
+	</div>
+	<div>
+		<p class="text-gray-600 text-sm mt-1">Checked 10:31:22 PM</p>
+	</div>
+</div>
